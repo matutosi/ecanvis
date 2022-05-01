e2ee6eb6f3b30f95f476511d1e1601dee4534993
matutosi <matutosi@gmail.com>
2022-05-02 07:41:47 +0900

    change page fluid -> tab

--- server.R
+++ server.R
@@ -3,16 +3,29 @@ function(input, output) {
   output$sccater_plot <- renderPlot({
     df_sample %>%
       ggplot(aes(x = !!input$var_1, y = !!input$var_2)) + 
-      geom_point()
+      geom_point() + 
+      theme_bw()
   })
 
-  bins <- reactive({
-    x = faithful[, 2]
-    return (seq(min(x), max(x), length.out = input$bins + 1))
+
+  output$table <- renderDataTable({
+    read_delim(input$file$datapath)
   })
 
-  output$distPlot <- renderPlot({
-    hist(faithful[, 2], breaks = bins(), col =  input$color, border = 'white')
+
+  output$clustering <- renderPlot({
+    input$cl_c_method
+    input$cl_d_method
+
   })
+
+  #   bins <- reactive({
+  #     x = faithful[, 2]
+  #     return (seq(min(x), max(x), length.out = input$bins + 1))
+  #   })
+  # 
+  #   output$distPlot <- renderPlot({
+  #     hist(faithful[, 2], breaks = bins(), col =  input$color, border = 'white')
+  #   })
 
 }
--- ui.R
+++ ui.R
@@ -1,21 +1,51 @@
-fluidPage(
-titlePanel("Sample data (dune and dune.env from vegan)"),
+  # fluidPage(
+navbarPage("ecan", 
+  #   titlePanel("Sample data (dune and dune.env from vegan)"),
 
-  sidebarLayout(
-    sidebarPanel(
-      
-      varSelectInput("var_1", "x: ", df_sample),
-      varSelectInput("var_2", "y: ", df_sample),
+  tabPanel("Sample data",
+    sidebarLayout(
+      sidebarPanel(
+        varSelectInput("var_1", "x: ", df_sample),
+        varSelectInput("var_2", "y: ", df_sample),
+      ),
+      mainPanel(
+        plotOutput("sccater_plot"),
+      )
+    )
+  ),
 
-  #       sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30),
-  #       selectInput("color", "select color", c("red", "blue", "green", "black"))
-    ),
-
-
-    mainPanel(
-  #       plotOutput("distPlot"),
-      plotOutput("sccater_plot")
-
+  tabPanel("Read file",
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("file", "choose file"),
+      ),
+      mainPanel(
+        dataTableOutput("table")
+      )
     )
   )
+
+  tabPanel("Clustering",
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("cl_c_method", "clustering method", 
+          choices = c("average", "ward.D", "ward.D2", "single", 
+                      "complete", "mcquitty", "median", "centroid", "diana")
+        ),
+
+        selectInput("cl_d_method", "distance method", 
+          choices = c("bray", "euclidean", "correlation", "manhattan", "canberra", 
+                      "clark", "kulczynski", "jaccard", "gower", "altGower", "morisita", 
+                      "horn", "mountford", "raup", "binomial", "chao", "cao", 
+                      "mahalanobis", "chisq", "chord", "aitchison", "robust.aitchison")
+        ),
+      )
+
+      mainPanel(
+        dataTableOutput("table")
+      )
+    )
+  )
+
+
 )
