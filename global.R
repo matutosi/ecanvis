dcee51181e1fecd842fd268eb5ae6a4f3051bf61
matutosi <matutosi@gmail.com>
2022-05-02 07:40:46 +0900

    rename st -> stand

--- global.R
+++ global.R
@@ -6,7 +6,8 @@ data(dune)
 data(dune.env)
 df_sample <- 
   dune %>%
-  tibble::rownames_to_column("st") %>%
-  tidyr::pivot_longer(-st, names_to = "species", values_to = "cover") %>%
+  tibble::rownames_to_column("stand") %>%
+  tidyr::pivot_longer(-!!"stand", names_to = "species", values_to = "cover") %>%
+  dplyr::filter(cover != 0) %>%
   dplyr::left_join(tibble::rownames_to_column(dune.env, "stand"))
 
