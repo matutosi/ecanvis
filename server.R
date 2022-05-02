ae0008f731fc8f251fc967a817d56a1ec1eb0b56
matutosi <matutosi@gmail.com>
2022-05-03 07:50:58 +0900

    use input data columns

--- server.R
+++ server.R
@@ -1,11 +1,7 @@
 
 function(input, output) {
 
-  data_file <- reactive({ 
-    if(is_null(input$file)) { return(NULL) }
-    else                    { read_delim(input$file$datapath) }
-  })
-
+  # Sample data
   output$sccater_plot <- renderPlot({
     df_sample %>%
       ggplot(aes(x = !!input$var_1, y = !!input$var_2)) + 
@@ -13,39 +9,29 @@ function(input, output) {
       theme_bw()
   })
 
+  # Input data
+  data_file <- reactive({ 
+    if(is_null(input$file)) { return(NULL) }
+    else                    { read_delim(input$file$datapath) }
+  })
+
   output$table <- renderDataTable({
     data_file()
   })
 
-  #   # For debug
-  #   output$table <- renderDataTable({
-  #      data_file() %>% dplyr::select(input$st, input$sp, input$ab)
-  #   })
-  #   output$stand <- renderText(input$st)
-
-
   output$clustering <- renderPlot({
     if(!is.null(data_file())){
       data_file() %>%
-  # need obserb() ?
-        df2table(st = input$st, sp = input$sp, ab = input$ab) %>%
-  #         df2table(st = "stand", sp = "species", ab = "cover") %>%
+        df2table(st = as.character(input$st), 
+                 sp = as.character(input$sp), 
+                 ab = as.character(input$ab)) %>%
         clustering(c_method = input$cl_c_method, d_method = input$cl_d_method) %>%
         plot()
     }
   })
 
-  output$st <- renderUI({
-    varSelectInput("st", "stand: " , data = data_file())
-  })
-
-  output$sp <- renderUI({
-    varSelectInput("sp", "species: " , data = data_file())
-  })
-
-  output$ab <- renderUI({
-    varSelectInput("ab", "abandance: " , data = data_file())
-  })
-
+  output$st <- renderUI({ varSelectInput("st", "stand: " ,    data = data_file()) })
+  output$sp <- renderUI({ varSelectInput("sp", "species: ",   data = data_file()) })
+  output$ab <- renderUI({ varSelectInput("ab", "abandance: ", data = data_file()) })
 
 }
