7f86fab56cb1582bd444ab94b1eb735de8286566
matutosi <matutosi@gmail.com>
2022-05-18 12:07:29 +0900

    Enable to select diversity index

--- R/diversity.R
+++ R/diversity.R
@@ -17,8 +17,12 @@ calculate_diversity <- function(data_in, st, sp, ab){
 diversityUI <- function(id, diversity){
   ns <- NS(id)
   tagList(
-  #     selectInput(session, "st_group", choices = colnames(diversity))
-
+    selectInput(ns("div_index"), "Diversity index",
+      choices = c("Species richness (s)" = "s",
+                  "Shannon's H' (h)"     = "h",
+                  "Simpson's D (d)"      = "d", 
+                  "Simpson's 1/d (i)"    = "i")
+    ),
     checkboxInput(ns("use_st_group"), "Use unit (stand) group", value = FALSE),
     selectInput(ns("st_group"), "unit group", choices = character(0)),
 
@@ -33,14 +37,16 @@ diversitySever <- function(id, diversity){
   moduleServer(id, function(input, output, session){
 
     observeEvent(input$use_st_group, ignoreInit = TRUE, {
-      updateSelectInput(session, "st_group", choices = colnames(diversity))
+      choices <- setdiff(colnames(diversity), c("s", "h", "d", "i"))
+      colnames(diversity)
+      updateSelectInput(session, "st_group", choices = choices)
     })
 
     output$diversity_plot_s <- renderPlot(res = 96, {
       if(input$use_st_group)
-        div_gg <- ggplot(diversity, aes(x = .data[[input$st_group]], y = s))
+        div_gg <- ggplot(diversity, aes(x = .data[[input$st_group]], y = .data[[input$div_index]]))
       else
-        div_gg <- ggplot(diversity, aes(x = 1,y = s))
+        div_gg <- ggplot(diversity, aes(x = 1,y = .data[[input$div_index]]))
       div_gg +
         geom_boxplot() + 
         geom_jitter(height = 0, width = 0.1) + 
