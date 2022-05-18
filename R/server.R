d5f3d32eb14b8cc63d29e798aea31288bc8d4f67
matutosi <matutosi@gmail.com>
2022-05-18 16:03:57 +0900

    activate cls 1-4, ord 1-4

--- R/server.R
+++ R/server.R
@@ -8,13 +8,6 @@ function(input, output, session){
   output$sp <- renderUI({varSelectInput("sp", "item (species): ",    data = data_in(), selected = colnames(data_in())[2]) })
   output$ab <- renderUI({varSelectInput("ab", "value (abandance): ", data = data_in(), selected = colnames(data_in())[3]) })
 
-  # stand group
-  #   use_select_varSever("st", data = data_in(), group_label = "unit group", selected_col = 6) # "st-gr")
-  # species group
-  #   id_sp <- "st"
-  #   use_select_varSever(id_sp, data = data_in(), group_label = "item group", selected_col = 1) # NS(id_st, "gr")
-
-
   # Download example
   output$download_example <-
     renderUI("Example data is generated with data dune and dune.env in library vegan.")
@@ -49,25 +42,16 @@ function(input, output, session){
   #   diversitySever("diversity", diversity(), input$"st-gr")
 
 
-  # # # Clustering (Dynamic) # # #
-  # IN PROGRESS (not work as expected)
-  #   output$clusters <- renderUI({
-  #     no_cls <- paste0("cls_", seq_len(input$no_cls))
-  #     print(no_cls) # for debug
-  #     map(no_cls, clusterUI)
-  #     map(no_cls, clusterSever, com_table)
-  #   })
-
   # # # Clusterings # # #
-  #   clusterSever("cls_1", com_table)
-  #   clusterSever("cls_2", com_table)
-  #   clusterSever("cls_3", com_table)
+  clusterSever("cls_1", com_table)
+  clusterSever("cls_2", com_table)
+  clusterSever("cls_3", com_table)
   clusterSever("cls_4", com_table)
 
   # # # Ordinations # # #
-  #   ordinationSever("ord_1", com_table)
-  #   ordinationSever("ord_2", com_table)
-  #   ordinationSever("ord_3", com_table)
+  ordinationSever("ord_1", com_table)
+  ordinationSever("ord_2", com_table)
+  ordinationSever("ord_3", com_table)
   ordinationSever("ord_4", com_table)
 
 }
--- R/ui.R
+++ R/ui.R
@@ -12,16 +12,10 @@ navbarPage("ecan",
         ),
         load_fileInput("load_file"),
 
-        # stand, species, cover, stand group, species group, 
+        # stand, species, cover, stand group, species group,
         uiOutput("st"),
         uiOutput("sp"),
         uiOutput("ab"),
-
-
-  #         use_select_varUI("st"),  # checkbox, select_var: input$"st-gr"
-  # not work
-  #         use_select_varUI("sp"),  # checkbox, select_var: input$"sp-gr"
-
 
       tags$hr(),
         htmlOutput("download_example"),
@@ -39,27 +33,19 @@ navbarPage("ecan",
     reactableOutput("diversity_table")
   ),
 
-  # # # Clustering (Dynamic) # # #
-  # IN PROGRESS (not work as expected)
-  #   tabPanel("Cluster",
-  #     numericInput("no_cls", "No. of clustering ( < 6 )",
-  #                  value = 2, min = 1, max = 5, step = 1,),
-  #     uiOutput("clusters")
-  #   ),
-
   # # # Clusterings # # #
   tabPanel("Clusterings",
-  #     clusterUI("cls_1"),
-  #     clusterUI("cls_2"),
-  #     clusterUI("cls_3"),
+    clusterUI("cls_1"),
+    clusterUI("cls_2"),
+    clusterUI("cls_3"),
     clusterUI("cls_4")
   ),
 
   # # # Ordinations # # #
   tabPanel("Ordinations",
-  #     ordinationUI("ord_1"),
-  #     ordinationUI("ord_2"),
-  #     ordinationUI("ord_3"),
+    ordinationUI("ord_1"),
+    ordinationUI("ord_2"),
+    ordinationUI("ord_3"),
     ordinationUI("ord_4")
   )
 )
