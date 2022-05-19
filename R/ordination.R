04259f2769635812b9c76072b8f350e1923b6e36
matutosi <matutosi@gmail.com>
2022-05-19 17:02:11 +0900

    adding group for ordination: almost finished!

--- R/ordination.R
+++ R/ordination.R
@@ -5,11 +5,11 @@ ordinationUI <- function(id){
     sidebarLayout(
       sidebarPanel(
         # method
-        selectInput(ns("or_o_method"), "ordination method",
+        selectInput(ns("ord_o_method"), "ordination method",
           choices = c("pca", "ca", "dca",
                       "pcoa", "fspa", "nmds")
         ),
-        selectInput(ns("or_d_method"), "distance method",
+        selectInput(ns("ord_d_method"), "distance method",
           choices = c("bray", "euclidean", "correlation", "manhattan",
                       "canberra", "clark", "kulczynski", "jaccard",
                       "gower", "altGower", "morisita", "horn",
@@ -18,26 +18,27 @@ ordinationUI <- function(id){
                       "robust.aitchison")
         ),
         # stand or species, x and y axis
-        radioButtons(ns("or_score"), "scores for plot",
-          choiceNames  = c("unit (stand)", "item (species)"),
-          choiceValues = c("st_scores",    "sp_scores")
+        selectInput(ns("ord_score"), "Scores for plot",
+            choices = c("Unit (stand)"   = "st_scores",
+                        "Item (species)" = "sp_scores")
+         ),
+  #         radioButtons(ns("ord_score"), "scores for plot",
   #           choiceNames  = c("unit (stand)", "item (species)", "both"),
-  #           choiceValues = c("st_scores",    "sp_scores",      "both")
-        ),
-        numericInput(ns("or_x"), "x axis component (1-4)",
+  #           choiceValues = c("st_scores",    "sp_scores",      "both"))
+        numericInput(ns("ord_x"), "x axis component (1-4)",
           value = 1, min = 1, max = 4, step = 1,
         ),
-        numericInput(ns("or_y"), "y axis component (1-4)",
+        numericInput(ns("ord_y"), "y axis component (1-4)",
           value = 2, min = 1, max = 4, step = 1,
         ),
 
-        selectInput(ns("use_group"), "Use group", 
-            choices = c("no"             = "no_group",
-                        "unit (stand)"   = "st_group",
-                        "item (species)" = "sp_group")
+        selectInput(ns("ord_use_group"), "Group", 
+            choices = c("No group"       = "ord_no_group",
+                        "Unit (stand)"   = "ord_st_group",
+                        "Item (species)" = "ord_sp_group")
 
         ),
-        selectInput(ns("group"), "unit group", choices = character(0)),
+        selectInput(ns("ord_group"), "Select group", choices = character(0)),
 
       ),
       mainPanel(
@@ -53,46 +54,50 @@ ordinationUI <- function(id){
 ordinationSever <- function(id, all_data){
   moduleServer(id, function(input, output, session){
 
-    observeEvent(input$use_group, ignoreInit = TRUE, {
+    observeEvent(input$ord_use_group, ignoreInit = TRUE, {
       choices <- all_data$cols
-      selected <- if(input$group == "") choices[1] else input$group
-      updateSelectInput(session, "group", choices = choices, selected = selected)
+      selected <- if(input$ord_group == "") choices[1] else input$ord_group
+      updateSelectInput(session, "ord_group", choices = choices, selected = selected)
     })
 
     output$ordination <- renderPlot(res = 96, {
 
-      req(all_data$com_table)
       ord <-
         all_data$com_table %>%
-        ordination(o_method = input$or_o_method, d_method = input$or_d_method)
+        ordination(o_method = input$ord_o_method, d_method = input$ord_d_method)
 
       single <- 
-        if(input$use_group == "st_group"){
+        if(input$ord_use_group == "ord_st_group"){
           all_data$st
-        } else if(input$use_group == "sp_group"){
+        } else if(input$ord_use_group == "ord_sp_group"){
           all_data$sp
         }
 
       ord_scores <- 
-        ord_add_group(
-        ord    = ord, 
-        score  = input$or_score,
-        df     = all_data$data_in,
-        single = single,
-        group  = input$group)
+        if(input$ord_use_group != "ord_no_group"){
+          ord_add_group(
+            ord    = ord, 
+            score  = input$ord_score,
+            df     = all_data$data_in,
+            single = single,
+            group  = input$ord_group)
+        } else {
+          ord_extract_score(ord, input$ord_score)
+        }
+
 print(ord_scores)
 
-      ggplot(ord_scores, aes(x = PC1, y = PC2, col = .data[[input$group]])) + 
-        geom_point()
-
   # devtools::load_all("d:/matu/work/todo/ecan/R")
-  # ord
-  #       all_data$st
-  #       all_data$sp
-  #       all_data$ab
-  #       all_data$cols
-  # print(input$group)
-  #       ord_plot(ord, score = input$or_score, x = input$or_x,  y = input$or_y, col = NULL)
+      x   <- names(ord_scores)[input$ord_x]
+      y   <- names(ord_scores)[input$ord_y]
+      gg <- ggplot2::ggplot(ord_scores, ggplot2::aes(.data[[x]], .data[[y]], label = rownames(ord_scores)))
+      if(input$ord_use_group != "ord_no_group"){
+        gg <- gg + ggplot2::geom_point(aes(col = .data[[input$ord_group]]), alpha=0.2, size = 7)
+        #   labs(col = input$ord_group) +  # change legend title
+      }
+      gg + 
+        ggplot2::geom_text() + 
+        ggplot2::theme_bw()
 
     })
   })
